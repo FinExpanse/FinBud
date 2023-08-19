4150d9bdc5fea19381edb3d3f2409bb9c6adc7fa
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2023-08-19 18:15:12 -0400

    Create App

---
+++ src/haxe/App.hx
@@ -0,0 +1,4 @@
+package;
+
+@:js.import('./app.jsx', "App")
+extern class App {}
--- src/haxe/Main.hx
+++ src/haxe/Main.hx
@@ -3,7 +3,6 @@ package;
 import js.html.Element;
 import react.ReactDOMClient;
 import styled.Styled;
-import app.App;
 
 function main() {
     var rootElement = document.getElementById("root");
